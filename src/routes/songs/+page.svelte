c141d9f899c2a8606255210c54a5b1ebcdf41846
Gerjan Gort <Gerjan@hollandmedicals.com>
2024-09-05 00:42:36 +0200

    working directory listing

--- src/routes/songs/+page.svelte
+++ src/routes/songs/+page.svelte
@@ -1,7 +1,19 @@
 <script lang="ts">
+    import {Button, Table, TableBody, TableBodyCell, TableBodyRow, TableHead, TableHeadCell} from "flowbite-svelte";
+    import {invoke} from "@tauri-apps/api/core";
+    import type {Song} from "../../lib/songs";
 
-    import {Table, TableHead, TableHeadCell} from "flowbite-svelte";
+    let songlist: Song[] = [];
+
+    async function updateSongs() {
+        songlist = await invoke('get_songs');
+    }
+
+    updateSongs();
+
 </script>
+
+<Button on:click={()=>updateSongs()}>Update</Button>
 
 <Table>
     <TableHead>
@@ -9,4 +21,13 @@
         <TableHeadCell>artist</TableHeadCell>
         <TableHeadCell>BPM</TableHeadCell>
     </TableHead>
+    <TableBody>
+        {#each songlist as song}
+            <TableBodyRow>
+                <TableBodyCell>{song.title}</TableBodyCell>
+                <TableBodyCell>artist</TableBodyCell>
+                <TableBodyCell>BPM</TableBodyCell>
+            </TableBodyRow>
+        {/each}
+    </TableBody>
 </Table>
