a952ddda5eaff1611d69b5f7c8bf2e6a1ddae405
Gerjan Gort <Gerjan@hollandmedicals.com>
2024-09-04 11:20:41 +0200

    working layout

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,21 @@
+<script lang="ts">
+    import {Navbar, NavBrand, NavUl, NavLi, NavHamburger, DarkMode} from "flowbite-svelte";
+
+    import '../app.css';
+
+
+</script>
+
+<Navbar>
+    <NavBrand>MTracks</NavBrand>
+    <div class="flex flex-row">
+        <NavUl>
+            <NavLi>Home</NavLi>
+        </NavUl>
+        <NavHamburger/>
+        <DarkMode/>
+
+    </div>
+</Navbar>
+
+<slot></slot>
